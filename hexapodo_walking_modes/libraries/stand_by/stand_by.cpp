47e84ab825a8aac8211eef2c478779981faec994
Jorge De León Rivas <jorge.dlr.88@gmail.com>
2017-12-11 14:33:53 +0100

    Stand Up
    Stand up completado para cuando el robot viene del suelo
    Faltaria si proviene de otro modo de marcha.

--- hexapodo_walking_modes/libraries/stand_by/stand_by.cpp
+++ hexapodo_walking_modes/libraries/stand_by/stand_by.cpp
@@ -14,35 +14,33 @@ void stand_by::move_legs()
       // TRIPOD TWO
       for(int i = 0; i < 3; i++)
       {
-        float error = (hexapodo_stand_by.pata[tripod_two.at(i)]->getPos() - (2 * PI - total_angle_rads / 2));
+        float error = (hexapodo_stand_by.pata[tripod_two.at(i)]->getPos() - (0 - total_angle_rads / 2));
         float velocity = -2.5 * error;
         hexapodo_stand_by.pata[tripod_two.at(i)]->setVelocity(velocity);
         ROS_INFO("Error de la pata %d = %.2f", i, error);
         if (abs(velocity) < 0.1)
         {
-          hexapodo_stand_by.pata[tripod_two.at(i)]->setPosition(2 * PI - (total_angle_rads / 2));
+          hexapodo_stand_by.pata[tripod_two.at(i)]->setPosition(0 - (total_angle_rads / 2));
           ROS_INFO("Posicion de la pata %d alcanzada", i);
         }
       }
 
       // TRIPOD ONE
-//      for(int i = 0; i < 3; i++)
-//      {
-//        float error = abs(hexapodo_stand_by.pata[tripod_one.at(i)]->getPos() - total_angle_rads / 2);
-//        float velocity = 2.5 * error;
-//        hexapodo_stand_by.pata[tripod_one.at(i)]->setVelocity(velocity);
-//        ROS_INFO("Error de la pata %d = %.2f", i, error);
+      for(int i = 0; i < 3; i++)
+      {
+        float error = abs(hexapodo_stand_by.pata[tripod_one.at(i)]->getPos() - total_angle_rads / 2);
+        float velocity = 2.5 * error;
+        hexapodo_stand_by.pata[tripod_one.at(i)]->setVelocity(velocity);
+        ROS_INFO("Error de la pata %d = %.2f", i, error);
 
-//        if (error < 0.1)
-//        {
-//          hexapodo_stand_by.pata[tripod_one.at(i)]->setPosition(total_angle_rads / 2);
-//          ROS_INFO("Posicion de la pata %d alcanzada", i);
-//        }
-//      }
-
-
-
+        if (error < 0.1)
+        {
+          hexapodo_stand_by.pata[tripod_one.at(i)]->setPosition(total_angle_rads / 2);
+          ROS_INFO("Posicion de la pata %d alcanzada", i);
+        }
+      }
     }
+
     else
     {
       legs_read_flag = hexapodo_stand_by.legs_readed();
